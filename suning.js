3ac719266dd91b98dfd2ff16a252cd471002b284
FannyGao <35021060+FannyGao@users.noreply.github.com>
2018-01-29 19:28:12 +0800

    Update suning.js

--- suning.js
+++ suning.js
@@ -7,16 +7,19 @@ window.onload=function () {
     let img=document.querySelectorAll('.b_img li');
     let left=document.querySelector('.jiantou_left');
     let right=document.querySelector('.jiantou_right');
+    let circle=document.querySelectorAll('.lunbodian li')
     let n=0;
     function move() {
+        n++;
         if(n==img.length){
             n=0;
         }
-        img.forEach(function (value) {
+        img.forEach(function (value,index) {
             value.classList.remove('active');
+            circle[index].classList.remove('act');
         })
         img[n].classList.add('active');
-        n++;
+        circle[n].classList.add('act');
     }
     let time=setInterval(move,2000);
     div.onmouseover=function () {
@@ -26,26 +29,30 @@ window.onload=function () {
         time=setInterval(move,2000);
     }
     right.onclick=function () {
-        n++;
-        if(n==img.length){
-            n=0;
-        }
-        img.forEach(function (value) {
-            value.classList.remove('active');
-        })
-        img[n].classList.add('active');
+        move()
     }
     left.onclick=function () {
         n--;
         if(n<0){
             n=img.length-1;
         }
-        img.forEach(function (value) {
+        img.forEach(function (value,index) {
             value.classList.remove('active');
+            circle[index].classList.remove('act');
         })
         img[n].classList.add('active');
+        circle[n].classList.add('act');
     }
-
+    circle.forEach(function (value,index) {
+        value.onclick=function () {
+            circle.forEach(function (val,ind) {
+                val.classList.remove('act');
+                img[ind].classList.remove('active');
+            })
+            this.classList.add('act');
+            img[index].classList.add('active');
+        }
+    })
 
     //change
     let chose=document.querySelectorAll('.dajuhui_top a.dajuhui_top_center');
